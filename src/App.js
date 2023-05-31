928c6e36e464c3dd16fd8e3ab47abfae714364bf
LeTrungTung <trungtung5484@gmail.com>
2023-05-31 15:33:48 +0700

    Tìm kiếm thông tin trên Github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import { useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [valueSearch, setValueSearch] = useState("");
+
+  // console.log(11, users);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch(`https://api.github.com/search/users?q=${valueSearch}`, {
+      method: "GET",
+      // headers: {
+      //   "Content-Type": "application/json",
+      // },
+      // body: JSON.stringify(data)
+    })
+      .then((res) => res.json())
+      .then((data) =>
+        data.items ? setUsers(data.items) : setUsers([])
+      )
+      .catch((err) => console.log(err));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Container>
+      <header>
+        <div>
+          <h3>Github Finder</h3>
+        </div>
+        <div id="right-header">
+          <h4>Home</h4>
+          <h4>About</h4>
+        </div>
       </header>
-    </div>
+
+      <form action="" onSubmit={handleSubmit}>
+        <div id="form-search">
+          <input
+            type="search"
+            placeholder="Search Users..."
+            id="id-search"
+            onChange={(e) => setValueSearch(e.target.value)}
+          />
+          <input type="submit" value="Search" id="id-submit" />
+        </div>
+      </form>
+      <hr />
+      <Container>
+        {users.length > 0 && (
+          <Row>
+            {users?.map((user, index) => {
+              return (
+                <Col lg="3" md="6" key={index} id="view-product">
+                  <Card style={{ width: "18rem" }}>
+                    <Card.Img variant="top" src={user.avatar_url} />
+                    <Card.Body>
+                      <Card.Title>{user.login}</Card.Title>
+                      <Card.Text>
+                        Some quick example text to build on the card
+                        title and make up the bulk of the card's
+                        content.
+                      </Card.Text>
+                      <Button variant="primary">Detail</Button>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              );
+            })}
+          </Row>
+        )}
+      </Container>
+    </Container>
   );
 }
 
